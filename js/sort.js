4840e2f6913601f134924ca72e53951e0a7f618a
Мария <maryreznik82@gmail.com>
2022-11-17 19:39:40 +0400

    Добавляет перерисовку при фильтрах

--- js/sort.js
+++ js/sort.js
@@ -1,62 +1,57 @@
 import { dataChanged } from './small-photo.js';
-import {dataComments} from './fullsize-photo.js';
+import { debounce, getRandomPhotos } from './util.js';
 
-// const sortBlock = document.querySelector('.img-filters');
-const filterDefault = document.querySelector('#filter-default');
-const filterRandom = document.querySelector('#filter-random');
-const filterDiscussed = document.querySelector('#filter-discussed');
-
-const discussedFunc = (photos) => {
-  const deletePrevious = document.querySelector('.img-filters__button--active');
-  deletePrevious.classList.remove('img-filters__button--active');
-  filterDiscussed.classList.add('img-filters__button--active');
-  photos.sort((a, b) => a.likes > b.likes ? -1 : 1);
-  console.log('photos discussed');
-  dataChanged(photos);
-  dataComments(photos);
-};
-
-const defaultFunc = (photos) => {
-  const deletePrevious = document.querySelector('.img-filters__button--active');
-  deletePrevious.classList.remove('img-filters__button--active');
-  filterDefault.classList.add('img-filters__button--active');
-  console.log('photos default');
-  dataChanged(photos);
-  dataComments(photos);
-};
-
-const randomFunc = (photos) => {
-  const deletePrevious = document.querySelector('.img-filters__button--active');
-  deletePrevious.classList.remove('img-filters__button--active');
-  filterRandom.classList.add('img-filters__button--active');
-
-  let newPhotos = [];
-
-  const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
-
-  while (newPhotos.length !== 10) {
-    const index = getRandomInt(photos.length);
-    newPhotos.push(photos[index]);
-    newPhotos = newPhotos.filter((v, i, arr) => arr.indexOf(v) === i);
-  }
-  dataChanged(newPhotos);
-  dataComments(newPhotos);
-  console.log(newPhotos);
-};
+const FILTER_DELAY = 500;
+const RANDOM_FILTER_COUNT = 10;
 
 const formFilter = document.querySelector('.img-filters__form');
 
+const setActiveButton = (button) => {
+  const activedButton = document.querySelector('.img-filters__button--active');
+  activedButton.classList.remove('img-filters__button--active');
+  button.classList.add('img-filters__button--active');
+};
 
-const allFilters = (photos) => {
-  formFilter.addEventListener('click', (evt) => {
-    if (evt.target === filterDiscussed) {
-      discussedFunc(photos);
-    } else if (evt.target === filterDefault) {
-      defaultFunc(photos);
-    } else if (evt.target === filterRandom) {
+const clearPhotoList = () => {
+  const list = document.querySelectorAll('.picture');
+  list.forEach((item) => item.remove());
+};
+
+const discussedFunc = (photos) => {
+  const discussedPhotos = photos.slice().sort((a, b) => a.likes > b.likes ? -1 : 1);
+  dataChanged(discussedPhotos);
+};
+
+const defaultFunc = (photos) => {
+  dataChanged(photos);
+};
+
+const randomFunc = (photos) => {
+  dataChanged(getRandomPhotos(photos, RANDOM_FILTER_COUNT));
+};
+
+const filterPhotoSwitch = debounce((photos, filterId) => {
+  clearPhotoList();
+  switch (filterId) {
+    case 'filter-random':
       randomFunc(photos);
+      break;
+    case 'filter-discussed':
+      discussedFunc(photos);
+      break;
+    case 'filter-default':
+      defaultFunc(photos);
+      break;
+  }
+}, FILTER_DELAY);
+
+const onClickFilters = (photos) => {
+  formFilter.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('img-filters__button')) {
+      setActiveButton(evt.target);
+      filterPhotoSwitch(photos, evt.target.id);
     }
   });
 };
 
-export {discussedFunc, defaultFunc, allFilters};
+export {discussedFunc, defaultFunc, onClickFilters};
